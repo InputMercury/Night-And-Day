4f0f27d8aa8182a2389098335ffc773b3ef141c4
Matt Elvidge <elvidgematthew@yahoo.com>
2019-07-30 15:27:41 -0400

    setup main display function
    added js content to switch from day to night with simple function

--- js/index.js
+++ js/index.js
@@ -1,3 +1,33 @@
-$(function(){
+/**$(function(){
   alert("hi");
+});
+**/
+$(document).ready(function(){
+  $('button').click(function(){
+    if($('body').hasClass('black')){
+      $('body').removeClass('black').addClass('white');
+
+    }
+    else{
+      $('body').removeClass('white').addClass('black');
+    }
+  });
+});
+
+$(document).ready(function(){
+  $('#orb').click(function(){
+    if($('#orb').hasClass('sun')){
+      $('#orb').removeClass('sun').addClass('moon');
+    }
+    else{
+      $('#orb').removeClass('moon').addClass('sun');
+    }
+
+    if($('#sky').hasClass('day')){
+      $('#sky').removeClass('day').addClass('night');
+    }
+    else{
+      $('#sky').removeClass('night').addClass('day');
+    }
+  });
 });
