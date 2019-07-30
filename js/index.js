002178a467f09a6a79a924f1ee9943c451122129
Matt Elvidge <elvidgematthew@yahoo.com>
2019-07-30 15:58:29 -0400

    version 0.5
    added more complex javascript(jquery) animation changes

--- js/index.js
+++ js/index.js
@@ -18,9 +18,17 @@ $(document).ready(function(){
   $('#orb').click(function(){
     if($('#orb').hasClass('sun')){
       $('#orb').removeClass('sun').addClass('moon');
+      $('#inner-orb').removeClass('inner-orb-visible');
+      $('#moonspot1').addClass('spot-visible');
+      $('#moonspot2').addClass('spot-visible');
+      $('#moonspot3').addClass('spot-visible');
     }
     else{
       $('#orb').removeClass('moon').addClass('sun');
+      $('#inner-orb').addClass('inner-orb-visible');
+      $('#moonspot1').removeClass('spot-visible');
+      $('#moonspot2').removeClass('spot-visible');
+      $('#moonspot3').removeClass('spot-visible');
     }
 
     if($('#sky').hasClass('day')){
@@ -28,6 +36,13 @@ $(document).ready(function(){
     }
     else{
       $('#sky').removeClass('night').addClass('day');
+    }
+
+    if($('#text').hasClass('day-color')){
+      $('#text').removeClass('day-color').addClass('night-color');
+    }
+    else{
+      $('#text').removeClass('night-color').addClass('day-color');
     }
   });
 });
